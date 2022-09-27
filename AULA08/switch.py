37b094232b7d0f9f43a62db1131a5552f7a838e9
Maria Gabriela <gabrielaanselmao@gmail.com>
2022-09-27 19:16:38 -0300

    commit
    commit

---
+++ AULA08/switch.py
@@ -0,0 +1,19 @@
+mes = 7
+
+match mes:
+    case 1:
+        print("domingo")
+    case 2:
+        print("segunda")
+    case 3:
+        print("terça-feira")
+    case 4:
+        print("quarta-feira")
+    case 5:
+        print("quinta-feira")
+    case 6:
+        print("sexta-feira")
+    case 7:
+        print("sábado")
+    case _:
+        print("feriado")
