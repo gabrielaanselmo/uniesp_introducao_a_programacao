7070722c63aaf6ee745ad948b8c79d21dd4b76b8
Maria Gabriela <gabrielaanselmao@gmail.com>
2022-10-22 19:25:15 -0300

    commit
    commit

--- AULA09/listas.py
+++ AULA09/listas.py
@@ -9,21 +9,21 @@ print(frutas[3])
 print(f"juliana gosta de {frutas[3]}")
 
 # Trocando de lugar
-print(frutas)
-frutas.insert(2, "Morango")
-print(frutas)
+#print(frutas)
+#frutas.insert(2, "Morango")
+#print(frutas)
 
 # Substituindo 
-frutas[0] ="Melancia"
-print(frutas)
+#frutas[0] ="Melancia"
+#print(frutas)
 
 # Deletando
-print(frutas)
-del frutas[3]
-print(frutas)
+#print(frutas)
+#del frutas[3]
+#print(frutas)
 
 # Procurando o índice
-indice_fruta = frutas.index("Melancia")
-print(f"O índice da fruta é {indice_fruta}")
-del frutas[indice_fruta]
-print(frutas)   
+#indice_fruta = frutas.index("Melancia")
+#print(f"O índice da fruta é {indice_fruta}")
+#del frutas[indice_fruta]
+#print(frutas)   
---
+++ TED10/ted10.py
@@ -0,0 +1,19 @@
+#Escreva um algoritmo que permita a leitura dos nomes de 10 clubes de futebol e armazene os nomes lidos em um vetor (lista). Após isto, o algoritmo deve permitir a leitura de mais 1 nome qualquer de clube e depois escrever a mensagem ACHEI, 
+# se o nome estiver entre os 10 nomes lidos anteriormente (guardados no vetor), ou NÃO ACHEI caso contrário
+
+# Lista
+futebol = ["flamengo", "corinthians", "chelsa", "barcelona", "botafogo", "palmeiras", "são paulo", "spt", "fluminense", "grêmio"]
+print(futebol[0])
+print(futebol[1])
+print(futebol[2])
+print(futebol[3])
+print(futebol[4])
+print(futebol[5])
+print(futebol[6])
+print(futebol[7])
+print(futebol[8])
+print(futebol[9])
+
+# Procurando o time de futebolk (time de futebol)
+print(f"juliana gosta de {frutas[3]}")
+
