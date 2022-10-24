0b81e40d4b2a8c2a8dd09938aabb3ad766aef8db
maria gabriela <gabrielaanselmao@gmail.com>
2022-10-24 09:46:04 -0300

    commit
    commit

--- TED10/ted10.py
+++ TED10/ted10.py
@@ -1,26 +1,24 @@
-#Escreva um algoritmo que permita a leitura dos nomes de 10 clubes de futebol e armazene os nomes lidos em um vetor (lista). Após isto, o algoritmo deve permitir a leitura de mais 1 nome qualquer de clube e depois escrever a mensagem ACHEI, 
-# se o nome estiver entre os 10 nomes lidos anteriormente (guardados no vetor), ou NÃO ACHEI caso contrário
+# Questão 01 -
 
-# Lista
-lista_futebol = []
+times = []
 
 while True:
-    op = int(input("1 - Adicionar time de futebol\n \
-                2 - Ler lista dos times \n \
-                3 - Sair \n \
-                Digite a opção: \n \
-                "))
-    
-    if op == 1:
-        # Adicionar objetos a lista
-        time = (input("Adicione um time de futebol: "))
-        lista_futebol.append(time)
+    clubes = input("Digite até 10 nomes de clubes de futebol: ")
 
-    elif op == 2:
-        # Lendo lista dos times
-        for lista in lista_futebol:
-            print(lista)
-            
-    elif op == 3:
+    if len(clubes) > 0:
+        times.append(clubes)
+        print(times)
+
+    if len(times) > 10:
+        print("Você já adicionou 10 clubes!")
         break
+
+    for clubes in times:
+        selecao = input("Quer que eu ache algum time? [SIM/NÃO]: ")
+        if selecao == "SIM":
+            part2 = input("Qual time você quer achar?:")
+            indice = times.index(part2)
+            print("Achei!")
+        elif selecao == "NÃO":
+            break
 
